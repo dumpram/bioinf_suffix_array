d5594cfffe2b948136f46bb878b12b815d2d2a36
Ivan Pavic <ivan.pavic2@fer.hr>
2017-01-12 23:39:06 +0100

    Added run test and dummy tests

---
+++ run_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+compile_script_name="compile"
+run_script_name="run"
+
+declare -a solution_dirs=("csharp" "java")
+
+for dir in "${solution_dirs[@]}"
+do
+    rm results-$dir.txt
+done
+
+for d in {2..10}
+do
+    for i in {1..10}
+        do
+            for dir in "${solution_dirs[@]}"
+            do
+                cd $dir
+                make d=$d in=../tests/test-$i.txt out=../out.txt >> \
+                    ../results-$dir.txt
+                cd ..
+                diff out.txt solutions/solution-$i.txt
+                verify=$?
+                if [ $verify -eq 0 ]; then
+                    echo "Verification of $dir for d=$d for test-$i successful!"
+                else
+                    echo "Verification of $dir for d=$d for test-$i failed!"
+                fi
+
+            done
+        done
+done
