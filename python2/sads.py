00f000faf01438ce79fec84f065c0f65091a239d
Bruno Rosan <zadraninn22@gmail.com>
2017-01-16 23:35:14 +0100

    Finished

--- python2/sads.py
+++ python2/sads.py
@@ -3,15 +3,13 @@ import pdb
 
 class SADS():
 
-    def __init__(self, input_file, min_dist):
+    def __init__(self, min_dist):
         '''
         Creates new instance of SA-DS object, capable of creating suffix array using SA-DS algorithm.
         '''
 
         assert min_dist >= 2
         self.min_dist = min_dist
-        self.read_data(input_file)
-        self.do_sads(self.input_data)
 
     def read_data(self, input_file):
         with open(input_file, 'r', encoding='utf-8') as f:
@@ -34,13 +32,21 @@ class SADS():
         self.input_length = len(input_string)
 
         self.distinguish_L_and_S()
-        print(self.t)
         self.find_critical_substrings()
-        print(len(self.p_1))
-        print(self.p_1)
-        self.bucket_sort()
-        print(len(self.s_1))
-        print(self.s_1)
+        duplicate_index = self.bucket_sort()
+
+        if duplicate_index:
+            recursive_sads = SADS(self.min_dist)
+            sa1 = recursive_sads.do_sads(self.s1)
+            return self.induce_sa(sa1)
+        else:
+            sa1 = [None] * len(self.s1)
+
+            for s1_index in range(len(self.s1)):
+                sa1[self.s1[s1_index]] = s1_index
+
+            return self.induce_sa(sa1)
+
 
     def distinguish_L_and_S(self):
         '''
@@ -50,12 +56,13 @@ class SADS():
         If S[i] > S[i+1] or S[i] = S[i+1] charcater is classified as L type.
         '''
 
-        self.t = [True]
+        self.t = [None] * self.input_length
         current_mark = True
         for index in reversed(range(len(self.input_string)-1)):
             if self.input_string[index] != self.input_string[index+1]:
                 current_mark = (self.input_string[index] < self.input_string[index+1])
-            self.t.insert(0,current_mark)
+            self.t[index] = current_mark
+        self.t[-1] = True 
 
     def find_critical_substrings(self):
         '''
@@ -73,28 +80,28 @@ class SADS():
         '''
 
         # find LMS characters first
-        lms_chars = []
+        self.lms = []
         s_found = False
-        for index in range(len(self.t)):
+        for index in range(1, len(self.t)):
             if self.t[index] and not s_found:
-                lms_chars.append(index)
+                self.lms.append(index)
                 s_found = True
                 continue
             if not self.t[index] and s_found:
                 s_found = False
 
-        self.p_1 = [lms_chars[0]]   # append only first for now, rest of them will be appended later
+        self.p1 = [self.lms[0]]   # append only first for now, rest of them will be appended later
 
         # find the rest of the critical characters
-        for lms_index in range(len(lms_chars)-1):
-            current_lms = lms_chars[lms_index]
-            next_lms = lms_chars[lms_index+1]
+        for lms_index in range(len(self.lms)-1):
+            current_lms = self.lms[lms_index]
+            next_lms = self.lms[lms_index+1]
 
             while (next_lms - current_lms - 1) > self.min_dist:     # append all d-crit chars between two lms chars
                 current_lms += self.min_dist
-                self.p_1.append(current_lms)
+                self.p1.append(current_lms)
 
-            self.p_1.append(next_lms)   # append lms as well
+            self.p1.append(next_lms)   # append lms as well
         
     def bucket_sort(self):
         '''
@@ -103,7 +110,7 @@ class SADS():
 
         omega = lambda i: 2 * self.input_string[i] + self.t[i]
 
-        p_1_ordered = list(zip(self.p_1, range(len(self.p_1))))  # we need indicies of elements inside P1 for later
+        p_1_ordered = list(zip(self.p1, range(len(self.p1))))  # we need indicies of elements inside P1 for later
 
         for current_d in reversed(range(self.min_dist+2)):       # for each index in substring, starting from LSD
             map_buckets = {}                                     # init buckets
@@ -131,25 +138,21 @@ class SADS():
             else:                                                # if this is the last pass create S1
                 s_index = 0
                 prev_substring = None
-                self.s_1 = [None] * len(self.p_1)                # generate S1 array from sorted crit indicies
+                self.s1 = [None] * len(self.p1)                # generate S1 array from sorted crit indicies
                 duplicate_index = False
                 for key_entry in key_entries_sorted:
                     for crit_entry in key_entry[1]:
                         current_substring = self.get_d_critical_substring(crit_entry[0])
+                        if prev_substring != None:
+                            if prev_substring != current_substring:
+                                s_index += 1
+                            else:
+                                duplicate_index = True
 
-                        if (prev_substring != None) and (prev_substring != current_substring):
-                            s_index += 1
-                        else:
-                            duplicate_index = True
-
-                        self.s_1[crit_entry[1]] = s_index
+                        self.s1[crit_entry[1]] = s_index
                         prev_substring = current_substring
                         
-                if duplicate_index:
-                    # recursive call
-                    # new_sads = SADS()
-                    # new_sads.do_sads(self.s_1)
-                    pass
+                return duplicate_index
 
     def get_d_critical_substring(self, index):
         '''
@@ -168,8 +171,66 @@ class SADS():
             return self.input_string[index:self.input_length] + (indices_missing * [self.input_string[-1]])
 
 
+    def create_buckets(self):
+        self.buckets_start = {}
+        for char in self.input_string:
+            if not char in self.buckets_start:
+                self.buckets_start[char] = 1
+            else:
+                self.buckets_start[char] += 1
+
+        char_buckets_sorted = sorted(self.buckets_start.items())
+        
+        self.buckets_start = {}
+        self.buckets_end = {}
+
+        count = -1
+        for map_entry in char_buckets_sorted:
+            self.buckets_start[map_entry[0]] = count + 1 
+            count += map_entry[1]
+            self.buckets_end[map_entry[0]] = count
+
+    def induce_sa(self, sa1):
+        self.sa = [-1] * self.input_length
+
+        self.create_buckets()
+
+        for sa1_index in range(len(sa1)-1, -1, -1):
+            p1_elem = self.p1[sa1[sa1_index]]
+            if p1_elem in self.lms:
+                self.sa[self.buckets_end[self.input_string[p1_elem]]] = p1_elem
+                self.buckets_end[self.input_string[p1_elem]] -= 1
+
+
+        self.create_buckets()
+
+        for sa_elem in self.sa:
+            if sa_elem > 0:
+                sa_elem_ = sa_elem - 1
+                if not self.t[sa_elem_]:
+                    self.sa[self.buckets_start[self.input_string[sa_elem_]]] = sa_elem_
+                    self.buckets_start[self.input_string[sa_elem_]] += 1
+
+
+        self.create_buckets()
+
+        for sa_elem in reversed(self.sa):
+            if sa_elem > 0:
+                sa_elem_ = sa_elem - 1
+                if self.t[sa_elem_]:
+                    self.sa[self.buckets_end[self.input_string[sa_elem_]]] = sa_elem_
+                    self.buckets_end[self.input_string[sa_elem_]] -= 1
+
+
+        return self.sa
+
+
 def main():
-    sads = SADS(sys.argv[1], int(sys.argv[2]))
+    sads = SADS(int(sys.argv[2]))
+    sads.read_data(sys.argv[1])
+    sa = sads.do_sads(sads.input_data)
+    for char in sa:
+        print(char)
 
 if __name__ == '__main__':
     main()
